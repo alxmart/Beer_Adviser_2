98f736e3d002b9af9812d57a83d201e02050642b
alxmart <alxmartinez@gmail.com>
2023-12-23 19:08:43 -0300

    Added viewBind config in gradle and code completed in MainActivity

--- app/src/main/java/com/luizafmartinez/beer_adviser_2/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/beer_adviser_2/MainActivity.kt
@@ -2,10 +2,39 @@ package com.luizafmartinez.beer_adviser_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
+import androidx.viewbinding.ViewBinding
+import com.luizafmartinez.beer_adviser_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.findBeer.setOnClickListener {
+
+            val theBeerColor: Spinner = binding.beerColor
+            val color = theBeerColor.selectedItem
+            val beerList = getBeers(color.toString())
+            val beers = beerList.reduce { str, item -> str + '\n' + item }
+            val brands = binding.brands
+            brands.text = beers
+        }
     }
+
+    private fun getBeers(color: String):List<String> {
+
+        return when (color) {
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
+        }
+    }
+
 }
